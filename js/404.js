30e0063b1a1569984120faf2928fe2a4bcd0f4ec
sabiyorp <sabiyorp@outlook.com>
2024-08-11 19:42:21 +0700

    Update

--- js/404.js
+++ js/404.js
@@ -14,3 +14,16 @@ function displayPath() {
 
 // Execute the displayPath function when the window finishes loading
 window.onload = displayPath;
+
+// Function to check screen width and show alert
+function checkScreenWidth() {
+    if (window.innerWidth <= 236) {
+        alert("Halaman tidak dapat dimuat dengan benar karena layar Anda terlalu kecil.\n\nGunakan layar yang lebih besar untuk melihat halaman ini.");
+    }
+}
+
+// Check screen width on page load
+window.addEventListener('load', checkScreenWidth);
+
+// Check screen width on window resize
+window.addEventListener('resize', checkScreenWidth);
